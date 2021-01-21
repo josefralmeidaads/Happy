934091d0ed75ff5a9f8a700c143c47e89d3fada1
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2021-01-21 10:13:56 -0300

    backend finalizado

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
   async create(request: Request, response: Response) {
@@ -34,7 +35,7 @@ export default {
       }
     });
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude: formatLatitude,
       longitude: formatLongitude,
@@ -43,7 +44,26 @@ export default {
       opening_hours,
       open_on_weekends: formatOpenOnWeekends,
       images
-     });
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Preenchimento do nome é obrigatório'),
+      latitude: Yup.number().required('Preenchimento da latitude é obrigatória'),
+      longitude: Yup.number().required('Preenchimento da longitude é obrigatória'),
+      about: Yup.string().required('Preenchimento das informações é obrigatória'),
+      instructions: Yup.string().required('Preenchimento das instruções é obrigatória'),
+      opening_hours: Yup.string().required('Preenchimento de horário de funcionamento é obrigatório'),
+      open_on_weekends: Yup.boolean().required('Preenchimento de horário de funcionamento no fim de semana é obrigatório'),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required('o caminho da imagem é necessário'),
+      })),
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
 
      await orphanagesRepository.save(orphanage);
      return response.status(201).json(orphanage);
--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,9 +1,24 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
 
-const errorHandler: ErrorRequestHandler = (error, request, respose, next) => {
+interface ValidationErrors {
+  [key: string]: string[];
+}
+
+const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
   console.error(error);
 
-  return respose.status(500).json({ message: 'Erro Interno do Servidor'});
+  if(error instanceof ValidationError){
+    let errors: ValidationErrors = {};
+
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+
+    return response.status(400).json({ message: 'Validation Fails', errors });
+  }
+
+  return response.status(500).json({ message: 'Erro Interno do Servidor'});
 };
 
 export default errorHandler;
